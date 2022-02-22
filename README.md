fcffd3fd6a8c65fd88158721eeb12e035bf92d66
unknown <Gintautas>
2022-02-22 12:37:59 +0200

    feat: Components

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # learning_react
+https://www.youtube.com/watch?v=bMknfKXIFA8
+21:00
 
 Run application with:
 > python3 -m http.server
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-ReactDOM.render(<h1>My HTML</h1>, document.getElementById("root"));
+function NavBar() {
+    return(<div>
+        <ul>
+            <li>Main</li>
+            <li>About</li>
+        </ul>
+    </div>)
+}
+
+function MainContent() {
+    return(<div>
+        <p>This is main content!</p>
+        <p>React is component based (Basicly you solve a puzle)</p>
+        <p>React is declarative (What should be done?)</p>
+        <p>Imperative - how it should be done?</p>
+    </div>)
+}
+
+ReactDOM.render(
+    <div>
+        <NavBar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+);
